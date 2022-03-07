91e67edfbd90c1816a2e2dcd0ac05844bb5d9ff2
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-03-07 16:47:49 +0200

    Setup the routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Rockets from "./Components/Rockets/Rockets";
-import Missions from "./Components/Missions/Missions";
-import MyProfile from "./Components/MyProfile/MyProfile";
-import NavBar from "./Components/NavBar/NavBar";
-import Dragon from "./Components/Dragon/Dragon";
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Rockets from './Components/Rockets/Rockets';
+import Missions from './Components/Missions/Missions';
+import MyProfile from './Components/MyProfile/MyProfile';
+import NavBar from './Components/NavBar/NavBar';
+import Dragon from './Components/Dragon/Dragon';
 
 function App() {
   return (
     <div className="App">
-
       <NavBar />
-      <Rockets />
-      <Missions />
-      <Dragon />
-      <MyProfile />
-      
+      <Routes>
+        <Route exact path="/" element={<Rockets />} />
+        <Route path="/dragon" element={<Dragon />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<MyProfile />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: '/dragon',
+      text: 'Dragon',
+    },
+    {
+      id: 3,
+      path: '/missions',
+      text: 'Missions',
+    },
+    {
+      id: 4,
+      path: '/profile',
+      text: 'My Profile',
+    },
+  ];
   return (
-    <div>NavBar</div>
-  )
-}
+    <header>
+      <div>
+        <img src="" />
+        <h1>Space traveler's Hub</h1>
+      </div>
+      <nav>
+        <ul>
+          {links.map((link) => {
+            <li key={link.id}>
+              <NavLink to={link.path}>
+                {link.text}
+              </NavLink>
+            </li>;
+          })}
+        </ul>
+      </nav>
+    </header>
 
-export default NavBar
+  );
+};
+
+export default NavBar;
