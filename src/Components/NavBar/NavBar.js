cf01173477e24eba74e613ee3285876d5463052a
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-03-07 17:02:23 +0200

    add logo image

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../../images/planet.png';
+import styles from './navBar.module.css'
 
 const NavBar = () => {
-  const links = [
+  const Links = [
     {
       id: 1,
       path: '/',
@@ -26,19 +28,19 @@ const NavBar = () => {
   ];
   return (
     <header>
-      <div>
-        <img src="" />
+      <div className='logo-container'>
+        <img src={logo} className='logo'/>
         <h1>Space traveler's Hub</h1>
       </div>
       <nav>
         <ul>
-          {links.map((link) => {
+          {Links.map((link) => (
             <li key={link.id}>
               <NavLink to={link.path}>
                 {link.text}
               </NavLink>
-            </li>;
-          })}
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
