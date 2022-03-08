9c0317062fd5d8c7679e3ac7e4aa6d1670012591
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-03-08 17:38:39 +0200

    Create the missions page

--- src/Components/Missions/Missions.js
+++ src/Components/Missions/Missions.js
@@ -1,7 +1,64 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions, toggleJoinMissions } from '../../Redux/missions/missions';
+import styles from './missions.module.css';
 
-const Missions = () => (
-  <div>Missions</div>
-);
+let fetched = false;
+const Missions = () => {
+  const missionsState = useSelector((state) => state.missionsReducer.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!fetched) {
+      dispatch(fetchMissions());
+      fetched = true;
+    }
+  }, []);
+
+  const clickHandler = (e) => {
+    const buttonId = e.target.getAttribute('data-target');
+    dispatch(toggleJoinMissions(buttonId));
+  };
+
+  return (
+    <table className={styles.table}>
+      <tbody>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+        {missionsState.map(
+          ({
+            mission_name: missionName,
+            mission_id,
+            description,
+            reserved,
+          }) => (
+            <tr key={mission_id}>
+              <td className={styles.missionName}>{missionName}</td>
+              <td>{description}</td>
+              <td>
+                <p>{reserved ? 'ACTIVE MEMBER' : 'NOT A MEMBER'}</p>
+              </td>
+              <td key={mission_id}>
+                <button
+                  data-target={mission_id}
+                  key={mission_id}
+                  type="button"
+                  onClick={clickHandler}
+                >
+                  {reserved ? 'Leave MIssion' : 'Join Mission'}
+                </button>
+              </td>
+            </tr>
+          ),
+        )}
+      </tbody>
+    </table>
+  );
+};
 
 export default Missions;
--- src/Components/MyProfile/MyProfile.js
+++ src/Components/MyProfile/MyProfile.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+const MyProfile = () => {
+  const missionsState = useSelector((state) => state.missionsReducer.missions);
+  const joinedMissions = missionsState.filter(
+    (missions) => missions.reserved === true,
+  );
+
+  return (
+    <div>
+      <h2>MIssions group</h2>
+      <ul>
+        {joinedMissions.map((mission) => (
+          <li key={mission.mission_id}>{mission.mission_name}</li>
+        ))}
+
+      </ul>
+    </div>
+  );
+};
 
 export default MyProfile;
