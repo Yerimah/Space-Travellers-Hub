4d0c7e917bdb4865abb210cf1d54ffbe4f86bab1
zieeco <zieecosam@gmail.com>
2022-03-09 20:24:46 +0100

    Hardcode the data

--- src/Components/MyProfile/MyProfile.js
+++ src/Components/MyProfile/MyProfile.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import styles from './myProfile.module.css';
 
 const MyProfile = () => (
-  <div>MyProfile</div>
+  <div className={styles.profile}>
+    <div className={styles.box}>
+      <h2 className="title">My Rockets</h2>
+      <table className="Mission-ProfileTable">
+        <tbody>
+          <tr><td className={styles['title-entry']}>Falcon 1</td></tr>
+          <tr><td className={styles['title-entry']}>Falcon 9</td></tr>
+          <tr><td className={styles['title-entry']}>Falcon Heavy</td></tr>
+          <tr><td className={styles['title-entry']}>Starship</td></tr>
+        </tbody>
+      </table>
+    </div>
+    <div className={styles.box}>
+      <h2 className="title">My Missions</h2>
+      <table className={styles['Mission-ProfileTable']}>
+        <tbody>
+          <tr><td className={styles['title-entry']}>Thaicom</td></tr>
+          <tr><td className={styles['title-entry']}>Telstar</td></tr>
+          <tr><td className={styles['title-entry']}>Iridium NEXT</td></tr>
+        </tbody>
+      </table>
+    </div>
+    <div className={styles.box}>
+      <h2 className="title">My Dragons</h2>
+      <table className="Mission-ProfileTable">
+        <tbody>
+          <tr><td className={styles['title-entry']}>Dragon 1</td></tr>
+          <tr><td className={styles['title-entry']}>Dragon 2</td></tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 );
 
 export default MyProfile;
