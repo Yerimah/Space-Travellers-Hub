7fe0a3f7f35d47c743816d8340d7265010a7e46d
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-08-29 18:35:54 +0200

    Embed video in the section

--- src/Components/HeadLine/HeadLine.js
+++ src/Components/HeadLine/HeadLine.js
@@ -4,14 +4,16 @@ import videoBg from '../../assets/stars-headline-bg.mp4';
 
 const HeadLine = () => {
   return (
-    <section className={styles['head-section']}>
-    <video className={styles['head__video']} autoPlay loop muted>
-        <source src={videoBg} type="video/mp4"/>
-    </video>
-    <div>HeadLine</div>
-    
+    <section className={styles["head-section"]}>
+      <div className={styles["head-video"]}>
+      <video className={styles["head-video__content"]} autoPlay loop muted>
+        <source src={videoBg} type="video/mp4" />
+      </video>
+      </div>
+      
+      <div>HeadLine</div>
     </section>
-  )
+  );
 }
 
 export default HeadLine;
