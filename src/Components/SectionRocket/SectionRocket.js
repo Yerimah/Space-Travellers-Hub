5477fa297dc65189306909b378db068dad96cead
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-08-30 11:24:57 +0200

    Add rockets section

---
+++ src/Components/SectionRocket/SectionRocket.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './sectionRocket.module.css';
+
+const SectionRocket = () => (
+  <section className={styles['rocket-section']}>
+    <div>
+      <h2>Travel tours by Rockets</h2>
+      <div>
+        <div>
+          <p>Lorem Ipsum</p>
+        </div>
+        <div>
+          Images
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default SectionRocket;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import HeadLine from '../Components/HeadLine/HeadLine';
+import SectionRocket from '../Components/SectionRocket/SectionRocket';
 
 const Home = () => (
   <>
     <HeadLine />
+    <SectionRocket />
   </>
 );
 
